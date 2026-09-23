885f03b1082186d43f5ffc850856a0792dd45318
Ethan Petersen <roboblockhead@gmail.com>
2026-09-22 22:01:20 -0500

    Made some fixes to SOTF and added some hardware map stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShootOnTheFly.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShootOnTheFly.java
@@ -4,56 +4,106 @@ import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.ManualDrive;
 import com.pedropathing.math.Pose;
 import com.pedropathing.math.Velocity;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.teamcode.pedro.Constants;
 
+import java.util.List;
+
+@TeleOp(name="SOTF", group="aaaOdometry")
 public class ShootOnTheFly extends OpMode {
 
     private final double GOAL_HEIGHT = 3; // Feet
     private final double GRAVITY = 32.1740; // Feet
-    private final double LAUNCH_HEIGHT = 1; // Feet
+    private final double LAUNCH_HEIGHT = .75; // Feet
     private final double LAUNCH_ANGLE = Math.toRadians(55);
-    private final Pose GOAL_POSE = new Pose(0, 0);
+    private final Pose GOAL_POSE = new Pose(11.8 / 12, 135.7 / 12); // x=129.7 for red
+    private boolean motorOn = false;
+    private boolean autoLocking = false;
+    private Servo flickerServo;
+    private DcMotorEx shooter;
 
     private Follower follower;
+    private Turntable turnable;
+
 
 
 
     @Override
     public void init() {
         follower = Constants.create(hardwareMap);
+        follower.setPose(new Pose(0, 0));
+        turnable = new Turntable(hardwareMap);
+        flickerServo = hardwareMap.get(Servo.class, "flicker");
+        shooter = hardwareMap.get(DcMotorEx.class, "shooter");
     }
 
     public void loop() {
         // Drive or hold automatically tries to resist movement when sticks are idle
-        ManualDrive.driveOrHold(follower,
-                -gamepad1.left_stick_y,
-                gamepad1.left_stick_x,
-                gamepad1.right_stick_x);
+        double rotate;
 
         // Update the follower to get a new position
         follower.update();
 
-        Pose adjustedTarget = getMovingShotTarget(follower.pose(), follower.velocity());
+        Pose robotPoseFeet = toFeet(follower.pose());
+        Velocity robotVelocityFeet = toFeetPerSecond(follower.velocity());
 
-        double adjustedVelocity = getBallVelocity(calculateDistance(follower.pose(), adjustedTarget));
-        double adjustedAngle = Math.atan2(adjustedTarget.y(), adjustedTarget.x());
+        Pose adjustedTarget = getMovingShotTarget(robotPoseFeet, robotVelocityFeet);
+
+        double adjustedVelocity = getBallVelocity(calculateDistance(robotPoseFeet, adjustedTarget));
+        double motorSpeed = getLaunchTPS(adjustedVelocity);
+        double adjustedAngle = Math.atan2(
+                adjustedTarget.y() - robotPoseFeet.y(),
+                adjustedTarget.x() - robotPoseFeet.x()
+        );
+
+        if (gamepad1.aWasPressed()) {motorOn = !motorOn;}
+        if (gamepad1.xWasPressed()) {autoLocking = !autoLocking;}
+        if (gamepad1.dpadRightWasPressed()) {turnable.turnRight();}
+        if (gamepad1.dpadLeftWasPressed()) {turnable.turnLeft();}
+        if (gamepad1.dpadUpWasPressed()) {
+            flickerServo.setPosition( 0.38);}
+        if (gamepad1.dpadDownWasPressed()) {flickerServo.setPosition(0.43);}
+
+        if (motorOn) {shooter.setVelocity(motorSpeed);}
+        if (autoLocking) {
+            rotate = turnToAngle(adjustedAngle, follower.pose().heading());
+        } else {
+            rotate = -gamepad1.right_stick_x;
+        }
+
+        ManualDrive.driveOrHold(follower,
+                -gamepad1.left_stick_y,
+                -gamepad1.left_stick_x,
+                rotate);
+
+        telemetry.addData("Adjusted Velocity", adjustedVelocity);
+        telemetry.addData("Adjusted Angle", adjustedAngle);
+        telemetry.addData("Motor Speed", motorSpeed);
+        telemetry.addData("Current Position", follower.pose());
+        telemetry.addData("Target Position", GOAL_POSE);
 
     }
 
-    public double getLaunchRPM() {
-        return 0;
+    public double getLaunchTPS(double velocity) {
+        double rpms =  (velocity * 120) / Math.PI * 0.314961;
+        return (rpms / 60) * 28;
     }
 
     Pose getMovingShotTarget(Pose robotPose, Velocity robotVelocity) {
+        double time = 1;
         // Start guessing at a true  target position
         double virtualX = GOAL_POSE.x() - (robotVelocity.vx * time);
         double virtualY = GOAL_POSE.y() - (robotVelocity.vy * time);
 
         double distance = calculateDistance(robotPose, new Pose(virtualX, virtualY));
         double launchVelocity = getBallVelocity(distance);
-        double time = calculateTime(distance, launchVelocity);
+        time = calculateTime(distance, launchVelocity);
 
         // Run three times to lock in on actual position
         for (int i = 0; i < 3; i++) {
@@ -82,5 +132,40 @@ public class ShootOnTheFly extends OpMode {
 
     public double calculateTime(double distance, double velocity) {
         return distance  / (velocity * Math.cos(LAUNCH_ANGLE));
+    }
+
+
+    public double turnToAngle(double targetHeading, double currentHeading) {
+        // Use tangent to calculate the angle needed to face the goal position on the field
+        double tolerance = Math.toRadians(1.5); // Tolerance in radians
+
+        // Deviation is the error between our current heading and the calculated target heading
+        double deviation = targetHeading - currentHeading;
+
+        // Normalize within -2(pi), 2(pi) so it doesn't try to spin multiple times
+        deviation = AngleUnit.normalizeRadians(deviation);
+
+
+        if (Math.abs(deviation) > tolerance) {
+            // Only run while error is outside of tolerance
+            double kP = .8; // Modify to change P strength
+            double turnPower = kP * deviation;
+
+
+            return Math.max(-0.6, Math.min(0.6, turnPower)); // Set max turning speed
+        } else {
+            // We are aligned, so command no turn.
+            return 0.0;
+        }
+    }
+
+    public Pose toFeet(Pose inchPose) {
+        return new Pose(inchPose.x() / 12.0, inchPose.y() / 12.0, inchPose.heading());
+    }
+
+    // Convert an inch-based Pedro Pathing Velocity vector to Feet per second
+    public Velocity toFeetPerSecond(Velocity inchVelocity) {
+        // Pedro Pathing's velocity is typically measured in inches per second
+        return new Velocity(inchVelocity.vx / 12.0, inchVelocity.vy / 12.0, inchVelocity.omega);
     }
 }
